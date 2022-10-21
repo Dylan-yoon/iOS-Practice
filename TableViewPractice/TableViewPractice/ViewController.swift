7f01d86c022c1eddc379b4a4c414a9cd6252d63f
Dylan-Yoon <d.dylany21@gmail.com>
2022-10-21 15:36:50 +0900

    feat: firstCommit TableViewPractice

---
+++ TableViewPractice/TableViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Byunghee_Yoon on 2022/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
