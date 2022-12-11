48f7a7a10d9c7eef9bdb184795966468105f55ab
dylan-yoon <d.dylany21@gmail.com>
2022-12-12 01:12:58 +0900

    feat: project 생성 및 주석 정리

---
+++ SingletonPrac/SingletonPrac/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SingletonPrac
+//
+//  Created by Dylan_Y on 2022/12/12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
---
+++ SingletonPrac/SingletonPrac/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SingletonPrac
+//
+//  Created by Dylan_Y on 2022/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
