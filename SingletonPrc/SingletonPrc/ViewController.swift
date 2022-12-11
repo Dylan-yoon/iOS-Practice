36e8855e2f9184c0316deca5e6dccc1b81ac08f6
Dylan-Yoon <d.dylany21@gmail.com>
2022-12-11 17:44:17 +0900

    feat: singletonPrc Project 생성

---
+++ SingletonPrc/SingletonPrc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingletonPrc
+//
+//  Created by Byunghee_Yoon on 2022/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
