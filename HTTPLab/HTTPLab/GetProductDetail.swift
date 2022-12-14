0014a416cdb76ea1be50ee5d21aee014550b10ff
dylan-yoon <d.dylany21@gmail.com>
2022-12-15 02:32:14 +0900

    feat: GetProductDetail 구현

--- HTTPLab/HTTPLab/GetProductDetail.swift
+++ HTTPLab/HTTPLab/GetProductDetail.swift
@@ -1,33 +1,30 @@
 //
-//  GetProducts.swift
+//  GetProductDetail.swift
 //  HTTPLab
 //
 //  Created by Dylan_Y on 2022/12/15.
 //
 
 import Foundation
+// https://openmarket.yagom-academy.kr/api/products/32
 
-struct GetProduct {
-    let baseURL = "https://openmarket.yagom-academy.kr"
-    let path = "/api/products" //?
-    let pageNo_Quary = "page_no=1" //&
-    let itemPerPage_Quary = "items_per_page=10"
-    
+struct GetProductDetail {
+    let urlString = "https://openmarket.yagom-academy.kr/api/products/32"
     let urlSession = URLSession.shared
     
     func fetchData(completion: @escaping (Data) -> Void) {
-        guard let url = URL(string: "https://openmarket.yagom-academy.kr/api/products?page_no=1&items_per_page=5") else {
+        guard let url = URL(string: urlString) else {
             return
         }
         
         urlSession.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print(error.localizedDescription)
+            if error != nil {
                 return
             }
             
             guard let response = response as? HTTPURLResponse,
                   (200..<300).contains(response.statusCode) else {
+                print("Response 오류")
                 return
             }
             
@@ -39,22 +36,9 @@ struct GetProduct {
     }
 }
 
-struct ProductList: Decodable {
-    let pageNo: Int
-    let itemsPerPage: Int
-    let totalCount: Int
-    let offset: Int
-    let limit: Int
-    let lastPage: Int
-    let hasNext: Bool
-    let hasPrev: Bool
-    let pages: [JsonArray]
-}
-
-struct JsonArray: Decodable {
+struct DetailList {
     let id: Int
     let vendor_id: Int
-    let vendorName: String
     let name: String
     let description: String
     let thumbnail: String
@@ -63,11 +47,20 @@ struct JsonArray: Decodable {
     let bargain_price: Double
     let discounted_price: Double
     let stock: Int
+    let images: [Images]
+    let vendors: Vendors
     let created_at: Date
     let issued_at: Date
 }
 
-enum Currency: Decodable {
-    case KRW
-    case USD
+struct Images: Decodable {
+    let id: Int
+    let url: String
+    let thumbnail_url: String
+    let issued_at: String
+}
+
+struct Vendors: Decodable {
+    let id: Int
+    let name: String
 }
