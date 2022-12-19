35d999cd1513789ae8010a72fcf78c301439551c
dylan-yoon <d.dylany21@gmail.com>
2022-12-20 02:53:41 +0900

    feat: ui기본셋팅

--- PriorityLab/PriorityLab/ViewController.swift
+++ PriorityLab/PriorityLab/ViewController.swift
@@ -8,12 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let firstLabel: UILabel = {
+        let label = UILabel()
+        label.text = "가만히 있는놈"
+        label.backgroundColor = .brown
+        
+        return label
+    }()
+    
+    let secondLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이것은 찌그러지지 않지만 그래도 쭉쭉 뻗어나가서 없어지는 마법의 텍스트"
+        label.backgroundColor = .blue
+        
+        return label
+    }()
+    
+    let thirdLabel: UILabel = {
+        let label = UILabel()
+        label.text = "누르지마ㅠㅠ"
+        label.backgroundColor = .systemPink
+        
+        return label
+    }()
+    
+    let topLabel: UILabel = {
+        let label = UILabel()
+        label.text = "그냥 있지요 위에서"
+        label.backgroundColor = .darkGray
+        
+        return label
+    }()
+    
+    lazy var horizontalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [firstLabel, secondLabel, thirdLabel])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+    
+    lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [topLabel, horizontalStackView])
+        stackView.backgroundColor = .lightGray
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        autolayout()
+        
     }
-
-
+    
+    func autolayout() {
+        view.addSubview(verticalStackView)
+        
+        NSLayoutConstraint.activate([
+            verticalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            verticalStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            verticalStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            verticalStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -200)
+        ])
+    }
 }
-
